2e804f6db312c648234498eeb9baa529c955f7f7
«Тумар <otumar@yandex.ru>
2022-06-11 00:29:06 +0300

    Task 03

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число => ");
+string paramString = Console.ReadLine();
+int Paramint = int.Parse(paramString);
+if (Paramint % 2 == 0)
+{
+    Console.WriteLine($"{Paramint} = четное");
+}
+else
+{
+    Console.WriteLine($"{Paramint} = нечетное");
+}
